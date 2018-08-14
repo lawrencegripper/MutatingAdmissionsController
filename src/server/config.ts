269cb678ef97ee0b4b005709685520ea83eaf3ad
lawrencegripper <info@grippers.co.uk>
2018-08-14 17:11:38 +0100

    WIP

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,5 +1,8 @@
+import * as fs from "fs";
 
 export const config = {
     port: process.env.NODE_PORT || 3000,
     rewriteContainerRepository: process.env.REWRITE_CONTAINER_REPOSITORY || "rewritten",
+    key: fs.readFileSync('./out/local-repository-controller-service.key', 'utf8'),
+    cert: fs.readFileSync('./out/local-repository-controller-service.crt', 'utf8')
 };
