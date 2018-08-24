9f053d1ee41fb001c8ab817f3e4af5e821720b9d
Scott Guymer <scott@scottguymer.co.uk>
2018-08-24 15:56:35 +0100

    Added in image pull secret config
    When rewriting to a private repository we need to add in the image pull secret for that repo

--- src/tests/deploymentCreate.spec.ts
+++ src/tests/deploymentCreate.spec.ts
@@ -4,6 +4,7 @@ import * as KoaRouter from 'koa-router';
 import { expect } from 'chai';
 import 'mocha';
 import * as fs from 'fs';
+import { config } from '../server/config'
 // import { request } from "https";
 
 function newMockContext() : KoaRouter.IRouterContext {
@@ -21,11 +22,24 @@ describe('DeploymentCreate', () => {
     it('shouldMutatePodImage', async () => {
         var context = newMockContext()
         context.request.body = JSON.parse(fs.readFileSync('./src/tests/data/deploymentCreateMutate.json', 'utf8'));
-        const response = await objectCreate(context)
+        await objectCreate(context)
 
         const expectedResponse = JSON.parse(fs.readFileSync('./src/tests/data/deploymentCreateMutate_response.json', 'utf8'));
         expect(context.body).to.deep.equal(expectedResponse);
     });
+
+    it('shouldAddImagePullSecret', async () => {
+      config.imagePullSecretName = "my-image-pull"
+      var context = newMockContext()
+      context.request.body = JSON.parse(fs.readFileSync('./src/tests/data/deploymentCreateMutate.json', 'utf8'));
+      await objectCreate(context)
+
+      var patch = JSON.parse(Buffer.from(context.body.response.patch, 'base64').toString('ascii'));
+
+      expect(patch[1]["op"]).to.equal("add");      
+      expect(patch[1]["path"]).to.equal("/spec/template/spec/imagePullSecrets");
+      expect(patch[1]["value"][0].name).to.equal("my-image-pull");
+  });
 
     it('shouldNotMutatePodImage', async () => {
       var context = newMockContext()
